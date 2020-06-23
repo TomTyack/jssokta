5870a22c78302c4875410ecd8ac9b8131dcd6341
Tom Tyack <thomastyack@aceik.com.au>
2020-06-23 12:38:05 +0000

    WIP on sign in widget

--- JSS/src/app/components/okta-sign-in/okta-sign-in.component.ts
+++ JSS/src/app/components/okta-sign-in/okta-sign-in.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ComponentRendering } from '@sitecore-jss/sitecore-jss-angular';
+import OktaSignIn from '@okta/okta-signin-widget';
+import { environment } from './../../../environments/environment';
+import { OktaAuthService } from '@okta/okta-angular';
+import { Router, NavigationStart } from '@angular/router';
 
 @Component({
   selector: 'app-okta-sign-in',
@@ -8,11 +12,56 @@ import { ComponentRendering } from '@sitecore-jss/sitecore-jss-angular';
 })
 export class OktaSignInComponent implements OnInit {
   @Input() rendering: ComponentRendering;
+  signIn;
+  widget: any;
+  constructor(oktaAuth: OktaAuthService, router: Router) { 
 
-  constructor() { }
+    if (typeof window === 'undefined') {
+      return;
+      // it's safe to use window now
+    }
+    this.widget = new OktaSignIn({
+      baseUrl: environment.okta.baseDomain,
+      authParams: {
+        pkce: true
+      }
+    });
+
+    this.signIn = oktaAuth;
+
+    // Show the widget when prompted, otherwise remove it from the DOM.
+    router.events.forEach(event => {
+      if (event instanceof NavigationStart) {
+        switch(event.url) {
+          case environment.okta.loginRoute:
+            break;
+          case environment.okta.portalRoute:
+            break;
+          default:
+            this.widget.remove();
+            break;
+        }
+      }
+    });
+  }
 
   ngOnInit() {
-    // remove this after implementation is done
-    console.log('okta-sign-in component initialized with component data', this.rendering);
+    if (typeof window === 'undefined') {
+      return;
+      // it's safe to use window now
+    }
+    this.widget.renderEl({
+      el: '#okta-signin-container'},
+      (res) => {
+        if (res.status === 'SUCCESS') {
+          this.signIn.loginRedirect('/', { sessionToken: res.session.token });
+          // Hide the widget
+          this.widget.hide();
+        }
+      },
+      (err) => {
+        throw err;
+      }
+    );
   }
 }
